f9a84d0be7ee4444ec9f4892fdb3a7323185d505
diegodox <android.mxdiego9@gmail.com>
2024-09-29 21:19:19 +0900

    add whickkey

--- config/default.nix
+++ config/default.nix
@@ -5,6 +5,16 @@ in
 {
   # Import all your configuration modules here
   imports = [
+    ./settings.nix
+    ./keymap.nix
+  ] ++ [
+    ./lsp.nix
+    ./cmp.nix
+    ./lualine.nix
+    ./tokyonight.nix
+    ./gitsigns.nix
+    ./noice.nix
+    ./whichkey.nix
   ] ++ nightly_module;
 
   wrapRc = true;
