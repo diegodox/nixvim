6121f3aa7552d6e0a3e55a789e3097ca005e8d10
diegodox <android.mxdiego9@gmail.com>
2024-11-04 22:09:15 +0900

    configuable fcitx5

--- config/fcitx5nvim.nix
+++ config/fcitx5nvim.nix
@@ -1,18 +1,34 @@
-{pkgs, plugin-fcitx5nvim, ...}: let
-  plugin = pkgs.vimUtils.buildVimPlugin {
-    name = "fcitx5";
-    src = plugin-fcitx5nvim;
-  };
-  config = /* lua */ ''
-  require("fcitx5").setup({
-    ime_on_status = "2",
-    ime_off_status = "1",
-    ime_on_arg = "-o",
-    ime_off_arg = "-c",
-  })
-  '';
+{ pkgs, lib, plugin-fcitx5nvim, ...}: let
+    plugin = pkgs.vimUtils.buildVimPlugin {
+      name = "fcitx5";
+      src = plugin-fcitx5nvim;
+    };
 in
 {
-  extraPlugins = [ plugin ];
-  extraConfigLua = config;
+  options = {
+    nixvim.fcitx5.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = ''
+        Whether enable fcitx5.nvim
+      '';
+    };
+  };
+
+  config = let
+    config = /* lua */ ''
+      -- Set up fcitx5.nvim {{
+      require("fcitx5").setup({
+        ime_on_status = "2",
+        ime_off_status = "1",
+        ime_on_arg = "-o",
+        ime_off_arg = "-c",
+      })
+      -- }}
+    '';
+  in lib.mkIf config.nixvim.fcitx5.enable {
+    extraPlugins = [ plugin ];
+    extraConfigLua = config;
+  };
 }
+
