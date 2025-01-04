4c401008b35c28540ddd926b9e9527ce65e6a47f
diegodox <android.mxdiego9@gmail.com>
2025-01-04 21:57:35 +0900

    fixup! nix flake update

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,6 +1,6 @@
 {
   plugins.lsp.enable = true;
 
-  plugins.lsp.servers.nil-ls.enable = true;
-  plugins.lsp.servers.nil-ls.autostart = true;
+  plugins.lsp.servers.nil_ls.enable = true;
+  plugins.lsp.servers.nil_ls.autostart = true;
 }
