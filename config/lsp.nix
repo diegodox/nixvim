f40bfa21249f7c4e8c8d4b98e703fbb506ba92ae
diegodox <android.mxdiego9@gmail.com>
2025-05-10 15:28:10 +0900

    nix format alejandra

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,8 +1,20 @@
-{ system, nixd, ... }:
+{ system, nixd, pkgs, ... }:
 {
   plugins.lsp.enable = true;
   plugins.lsp.inlayHints = true;
 
+  plugins.lsp.onAttach = /* lua */''
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
+  '';
   plugins.lsp.servers.basedpyright.enable = true;
   plugins.lsp.servers.ruff.enable = true;
   plugins.lsp.servers.lua_ls.enable = true;
@@ -10,8 +22,11 @@
   plugins.lsp.servers.nixd = {
     enable = true;
     package = nixd;
+    settings = {
+      formatting.command = [ "alejandra" ];
   };
   extraPackages = [
     nixd.packages.${system}.nixd
+    pkgs.alejandra
   ];
 }
