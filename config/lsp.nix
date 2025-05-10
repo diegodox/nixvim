2a8c8e30ce682cbbe3a747488fea8f73ee379aa2
diegodox <android.mxdiego9@gmail.com>
2025-05-10 14:53:41 +0900

    remove lsp attach message

--- config/lsp.nix
+++ config/lsp.nix
@@ -1,28 +1,7 @@
 { system, nixd, ... }:
 {
   plugins.lsp.enable = true;
-
-  plugins.lsp.onAttach = /* lua */ ''
-    -- LSP format autocommand {{{
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("LspFormatting", { clear = false }),
-        callback = function()
-            vim.lsp.buf.format({
-                bufnr = bufnr,
-            })
-        end,
-        desc = "Format buffer just before write",
-        buffer = bufnr,
-    })
-    vim.notify(
-        "lsp formater " .. client.name .. " set to buffer " .. bufnr .. ": " .. vim.api.nvim_buf_get_name(bufnr),
-        vim.log.levels.TRACE
-    )
-    -- }}}
-  '';
-
   plugins.lsp.inlayHints = true;
-
 
   plugins.lsp.servers.basedpyright.enable = true;
   plugins.lsp.servers.ruff.enable = true;
