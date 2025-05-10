ac540111cca563d0ab9c2a605ab69576427ceeb5
diegodox <android.mxdiego9@gmail.com>
2025-05-10 22:20:59 +0900

    nixd

--- config/lsp.nix
+++ config/lsp.nix
@@ -31,4 +31,49 @@
     nixd.packages.${system}.nixd
     pkgs.alejandra
   ];
+
+  extraConfigLua = /*lua*/''
+    require("lspconfig").nixd.setup({
+      settings = {
+        nixd = {
+          inlay_hints = true,
+          formatting = {
+            command = { "alejandra" },
+          },
+          nixpkgs = {
+            expr = string.format(
+              '(builtins.getFlake "%s").inputs.nixpkgs {}',
+              vim.fn.getcwd() ~= "" and vim.fn.getcwd() or "/home/cdockter/myNixOS"
+            ),
+          },
+          options = {
+            nixOptions = {
+              expr = string.format(
+                '(builtins.getFlake "%s").nixosConfigurations.nixos.options',
+                vim.fn.getcwd()
+              ),
+            },
+            home_manager = {
+              expr = string.format(
+                '(builtins.getFlake "%s").homeConfigurations."cdockter".options',
+                vim.fn.getcwd()
+              ),
+            },
+            flake_parts = {
+              expr = string.format(
+                '(builtins.getFlake "%s").debug.options',
+                vim.fn.getcwd() ~= "" and vim.fn.getcwd() or "/home/cdockter/myNixOS"
+              ),
+            },
+            perSystem = {
+              expr = string.format(
+                '(builtins.getFlake "%s").currentSystem.options',
+                vim.fn.getcwd() ~= "" and vim.fn.getcwd() or "/home/cdockter/myNixOS"
+              ),
+            },
+          },
+        },
+      },
+    })
+  '';
 }
